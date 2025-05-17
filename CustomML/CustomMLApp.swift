088fa662acc24d0e4548412444a812924f374617
Adeel Ahmad <adeel.ahmad@geant.org>
2025-05-17 11:34:03 +0100

    Initial Commit

---
+++ CustomML/CustomMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMLApp.swift
+//  CustomML
+//
+//  Created by Adeel Ahmad on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
